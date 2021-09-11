63cda3157a5d6e976b49e3361595a216f326a998
Dennis Moha <dennismoha2@gmail.com>
2021-09-11 07:26:07 +0300

    rectified client '/' buuild'

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(expressValidator());
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.static(path.join(__dirname, "public")));
 
 
